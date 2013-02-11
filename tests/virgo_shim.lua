2f388fd4e386fe14cdf3030e12c85c59c8c7bb96
Matt Kaniaris <mkaniaris@gmail.com>
2013-02-11 11:32:48 -0800

    adding a virgo shim

---
+++ tests/virgo_shim.lua
@@ -0,0 +1,19 @@
+
+virgo = {
+	force_crash = function: 0x402aea70,
+	default_config_filename = "rackspace-monitoring-agent.cfg",
+	version = "985b64bfc2bb86c60b08bfb17229a12031fd19f2",
+	loaded_zip_path = "/racker/virgo/out/Debug/monitoring-test.zip",
+	os = "linux",
+	config_path = "/racker/virgo/agents/monitoring/tests/fixtures/monitoring-agent-localhost.cfg",
+	agent_config = { version = "0.1.4.4" },
+	platform = "ubuntu-12.10-x86_64",
+	gmtnow = function: 0x402aeaf8,
+	force_dump = function: 0x402aeb40,
+	default_name = "rackspace-monitoring-agent",
+	config = {
+		monitoring_endpoints = "127.0.0.1:50041,127.0.0.1:50051,127.0.0.1:50061",
+		monitoring_token = "******",
+		monitoring_id = "agentA"
+	}
+}
