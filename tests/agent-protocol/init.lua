f584fffd072c94bce0921faf515c715fee0b8230
Matt Kaniaris <mkaniaris@gmail.com>
2013-02-28 16:43:34 -0800

    fix namespace resolution of loaded fixtures several more tests now pass

--- tests/agent-protocol/init.lua
+++ tests/agent-protocol/init.lua
@@ -27,6 +27,10 @@ local fixtures = require('/tests/fixtures')
 
 local exports = {}
 
+local prepareJson = function(msg)
+  return JSON.stringify(msg):gsub('\n', " ")
+end
+
 exports['test_completion_key'] = function(test, asserts)
   local sock = Emitter:new()
   local conn = AgentProtocolConnection:new(loggingUtil.makeLogger(), 'MYID', 'TOKEN', 'GUID', sock)
@@ -97,7 +101,7 @@ exports['test_unexpected_response_and_hello_timeout'] = function(test, asserts)
 
   msg.id = 4
 
-  data = fixtures.prepareJson(msg)
+  data = prepareJson(msg)
   sock.write = function()
     sock:emit('data', data .. "\n")
   end
--- tests/fixtures/init.lua
+++ tests/fixtures/init.lua
@@ -1,25 +1,16 @@
 local path = require('path')
-local fs = require('fs')
 local string = require('string')
-local JSON = require('json')
-local vutils = require('virgo_utils')
 
 local statics = require('/lua_modules').statics
-
-local fixtures = nil
-
-local strip_newlines = function(str)
-  return str:gsub("\n", " ")
-end
 
 local load_fixtures = function(dir)
   local fixtures = {}
 
   for i,v in ipairs(statics) do
     if path.posix:dirname(v) == dir then
-      local _, _, name = string.find(v, '(.*).json')
+      local _, _, name = path.posix:basename(v):find('(.*).json')
       if name ~= nil then
-        fixtures[name] = strip_newlines(get_static(v))
+        fixtures[name] = get_static(v):gsub("\n", " ")
       end
     end
   end
@@ -33,11 +24,6 @@ exports['invalid-version'] = load_fixtures(path.join(base, 'invalid-version'))
 exports['invalid-process-version'] = load_fixtures(path.join(base, 'invalid-process-version'))
 exports['invalid-bundle-version'] = load_fixtures(path.join(base, 'invalid-bundle-version'))
 exports['rate-limiting'] = load_fixtures(path.join(base, 'rate-limiting'))
-
-exports.prepareJson = function(msg)
-  local data = JSON.stringify(msg)
-  return strip_newlines(data)
-end
 
 exports.TESTING_AGENT_ENDPOINTS = {'127.0.0.1:50041', '127.0.0.1:50051', '127.0.0.1:50061'}
 return exports
